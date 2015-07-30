53227445977dfc0bf3f3af6811ada2c046dc985f
jordajm <jordajm@gmail.com>
2015-07-30 07:15:18 +0000

    finally??

--- schema/Org.js
+++ schema/Org.js
@@ -3,6 +3,7 @@
 exports = module.exports = function(app, mongoose) {
   var orgSchema = new mongoose.Schema({ 
     orgName: String,
+    orgLink: String,
     orgAPIKey: String,
     orgType: String,
     orgIrisMetricIds: [String],
